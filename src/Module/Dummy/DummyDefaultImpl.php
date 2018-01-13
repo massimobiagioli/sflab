a72e359fbe99fe524a22bb7762b15512c9ff71f1
Massimo Biagioli <biagiolimassimo@gmail.com>
2018-01-13 15:58:21 +0100

    Correzioni

--- src/Module/Dummy/DummyDefaultImpl.php
+++ src/Module/Dummy/DummyDefaultImpl.php
@@ -6,12 +6,9 @@ use App\Module\Dummy\Dummy;
 
 class DummyDefaultImpl implements Dummy {
     
-    private $counter;
-    
     public function execute()
     {
-        $this->counter++;
-        return 'Executed from Default Dummy Implementation' . $this->counter;
+        return "Executed from Default Dummy Implementation";
     }
 
 }
